492bbae36378180cba0efcb74c7001347c5c683c
DeeDeeR <david.dellarossa@gmail.com>
2026-04-08 18:14:30 +0200

    Add movement logic for paddles

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,20 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let pl = Paddle::new(Side::Left);
+    let mut pl = Paddle::new(Side::Left);
+    let mut pr = Paddle::new(Side::Right);
+    
     loop {
-        // let dt = get_frame_time();   // 1. timing                                                                                                                                                                                                                                                                     
+        let dt = get_frame_time().min(0.05);   // 1. timing                                                                                                                                                                                                                                                                     
 
         // 2. input                                                                                                                                                                                                                                                                                                   
-        // read keys, mouse, etc.                                                                                                                                                                                                                                                                                     
+        // read keys, mouse, etc.  
+        if is_key_down(KeyCode::W) { pl.move_up(dt)}
+        if is_key_down(KeyCode::S) { pl.move_down(dt)}
+        if is_key_down(KeyCode::Up) { pr.move_up(dt)}
+        if is_key_down(KeyCode::Down) { pr.move_down(dt)}
+        pl.clamp_to_screen();
+        pr.clamp_to_screen();
 
         // 3. update                                                                                                                                                                                                                                                                                                  
         // move things, check collisions, update score, etc.
@@ -35,6 +43,7 @@ async fn main() {
         // draw everything here
         // draw_rectangle(20.0, 260.0, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
         pl.draw();
+        pr.draw();
 
         next_frame().await;          // 5. present frame — always last
 
